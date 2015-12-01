f78835ebe57da40ce59bf7661d6cddd48b337a6f
FrancescoSTL <francescosapolizzi@gmail.com>
2015-12-01 16:24:21 -0600

    Send arr of JSON objects to client.

--- public/assets/js/build.js
+++ public/assets/js/build.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
         });
     }());
 
+
 /*
     var projectsList = JSON.stringify(projToClient)
 
--- routes/index/index.js
+++ routes/index/index.js
@@ -5,9 +5,20 @@ var router = express.Router();
 require('../../process.js')(function (projectsJSON) {
   // This code runs once the passwords have been loaded.
 
-	console.log(projectsJSON);
+	//console.log(projectsJSON);
+
+	
 
 	router.get('/', function(request, response) {
+		var allOptions = "";
+	    /*for(var i=0; i<projectsJSON.length; i++)
+	    {
+	    	//loop through all projects and add an option, then return string with all of that html in it
+	    	allOptions += "<option value=\"" + projectsJSON[i].id + "\">"+projectsJSON[i].name+"</option>";
+	    }*/
+	    console.log(projectsJSON);
+
+	    //pass our array of JSON objects to the client
 	    response.render('index.html', projectsJSON);
 	});
 });
