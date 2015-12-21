0f5ff6734ef3d8ecd2f3c8cd7eff31ce233dedd4
FrancescoSTL <francescosapolizzi@gmail.com>
2015-12-21 16:32:32 -0600

    Add ability to create experiment, update schedule, and delete default variations.

--- process/process.js
+++ process/process.js
@@ -1,7 +1,11 @@
 'use strict';
 
-// PROCESS.JS
-// Purpose: To asynchronously make optimizely client API calls based upon the current step
+// Written by: Francesco Polizzi
+// Date: December 15, 2015
+// Purpose: To asynchronously make optimizely client API calls based upon the current step in the test creation.
+// Input parameters: callback - a function which is executed once the asynchronous call is completed
+//                   step - An integer value used to segment different calls needed at different steps in the test creation process
+//                   projectID - The ID for the project selected by the user
 
 var access = require('../access.js');
 var OptimizelyClient = require('optimizely-node-client');
@@ -13,23 +17,26 @@ var oc = new OptimizelyClient(API_TOKEN);
 //initialize JSON object
 var data = [{}];
 
-var projects = oc.getProjects();
-
-
 module.exports = {	
-	initialize: function (callback, step, projectID) { 
+	initialize: function (callback, step, projectID, goal, audience, expTitle, editorURL, experimentURL, isRegex, pctVisitors, varTitle, varPercent, customJS, startTime, stopTime) { 
 		switch(step)
 		{
 			case 0:
 			{
+				// get projects
+				var projects = oc.getProjects();
+
+				// get response from projects
 				projects.done(function(projectsList) {
 					var x=0;
-				    projectsList.forEach(function(project) {
-				    	var projectObj = {name : project.project_name, id : project.id };
-				    	data[x] = projectObj;
-				    	x++;
-				    });
-				    callback(data);
+					// for each object in the json objects array, add that object to the data array
+					projectsList.forEach(function(project) {
+						var projectObj = {name : project.project_name, id : project.id };
+						data[x] = projectObj;
+						x++;
+					});
+					// return projects to callback
+					callback(data);
 				});
 				break;
 			}
@@ -64,11 +71,93 @@ module.exports = {
 			                data[x] = audienceObj;
 			                x++
 			            });
-			            // return goals + audiences
+			            // return goals + audiences to callback
 			            callback(data);
 			        });
 		        });
 		        break;
+			}
+			case 4: 
+			{
+				if(isRegex == "true")
+				{
+					var expUrlConditions = [{
+                            "match_type": "regex",
+                            "value": editorURL
+                        }];
+				}
+				else
+					var expUrlConditions = [{
+                            "match_type": "simple",
+                            "value": editorURL
+                        }];
+
+                var expVariations = [""];
+                var expAudienceIDs = [];
+
+				// Create experiment
+                var experimentPosted = oc.createExperiment({
+                    project_id: projectID,
+                    description: expTitle,
+                    status: "Not started",
+                    url_conditions: expUrlConditions,
+                    edit_url: experimentURL,
+                    activation_mode: "immediate",
+                    experiment_type: "ab",
+                    variation_ids: expVariations
+                });
+
+                // get info for experiment just created
+                experimentPosted.then(function(experimentDetails) {
+                	var experimentID = experimentDetails.id;
+                    expAudienceIDs[expAudienceIDs.length] = parseInt(audience);
+
+                    // update our created experiment with these details
+                    var update = oc.updateExperiment({
+                        id: experimentID,
+                        audience_ids: expAudienceIDs,
+                        percentage_included: pctVisitors*100
+                    });
+
+                    // when we are done updating the experiment
+                    update.done(function(result) { 
+                    	console.log("Updated experiment...\n");
+
+                    	// if we have a start or stop time to add
+						if(startTime || stopTime)
+                        {
+							//Create schedule for experiment w/ experiment ID
+							var createSchedule = oc.createSchedule({
+								"stop_time": stopTime,
+								"start_time": startTime,
+								"experiment_id": experimentID
+							});
+
+							createSchedule.done(function(result) { console.log("Added schedule..."); });
+						}
+
+						var variationsList = oc.getVariations({
+							id: experimentID
+						});
+
+						variationsList.then(function(variations) {
+							console.log("Retrieved default variations...");
+
+							variations.forEach(function(variation) {
+								var deleted = oc.deleteVariation({
+									id: variation.id
+								});
+							});
+
+							console.log("Deleted default variations...");
+
+
+						});
+
+					});
+				});
+
+				break;
 			}
 
 		}
--- routes/index/index.js
+++ routes/index/index.js
@@ -10,6 +10,7 @@ var stepPage = 0;
 
 		switch(stepPage)
 		{
+			// if we are trying to render the first page
 			case 0:
 			{
 					// asynchronously request our projects/project IDs
@@ -19,14 +20,17 @@ var stepPage = 0;
 					request.session.projects = projects;
 					// render our page with those projects
 					response.render('index.html', {projects: projects, step: stepAPI});
-				}, stepAPI);
+				}, 
+				stepAPI);
 
+				// set the page step
 				stepPage = 1;
 				break;
 			}
+			// if we are trying to render the second page
 			case 1:
 			{
-				// set the step
+				// set the step for the API process
 				stepAPI = 1;
 
 				// store that project in memory
@@ -35,22 +39,77 @@ var stepPage = 0;
 
 				// render our page with projects, chosen project, and step
 				require('../../process/process.js').initialize(function (goalAudience) {
-
-					// next step, parse goalAudience into goals and audiences respectively
-					// gotta save that info into session and then render the template with it for the user
-
+					// set the step for the API process
 					stepAPI = 2;
 
+					// parse goalAudience for our separated goals and audiences
 					request.session.goals = parseGoals(goalAudience);
 					request.session.audiences = parseAudiences(goalAudience);
 
-					response.render('index.html', {projects: request.session.projects, step: stepAPI, chosenProj: ProjID, audiences: request.session.audiences, goals: request.session.goals});
+					// render the page
+					response.render('index.html', {step: stepAPI, audiences: request.session.audiences, goals: request.session.goals});
 				
-				}, stepAPI, request.session.projectID);
+				}, 
+				stepAPI, 
+				request.session.projectID);
 
+				// set the page step
 				stepPage = 2;
 				break;
 			}
+			// if we are trying to render the second page
+			case 2:
+			{
+				stepAPI = 3;
+				// get querystring parameters from our form submit and store them in the session
+				request.session.expTitle = request.query.exptitle;
+				request.session.editorURL = request.query.editorURL;
+				request.session.experimentURL = request.query.experimentURL;
+				request.session.isRegex = request.query.isRegex;
+				request.session.pctVisitors = request.query.pctVisitors;
+				// we will default the number of variations to 2 for right now
+				// request.session.numVariations = request.query.numVariations;
+				request.session.audience = request.query.audience;
+				request.session.goal = request.query.goals;
+
+				// render the page
+				response.render('index.html', {step: stepAPI});
+
+				stepPage = 3;
+				break;
+			}
+			case 3:
+			{
+				stepAPI = 4;
+				// get querystring parameters from our form submit and store them in the session
+				request.session.varTitle = request.query.vartitle;
+				request.session.varPercent = request.query.varPercent;
+				request.session.customJS = request.query.customJS;
+				request.session.startTime = request.query.startTime;
+				request.session.stopTime = request.query.stopTime;
+
+				require('../../process/process.js').initialize(function (data) {
+
+				}, 
+				stepAPI, 
+				request.session.projectID, 
+				request.session.goal, 
+				request.session.audience, 
+				request.session.expTitle, 
+				request.session.editorURL, 
+				request.session.experimentURL,
+				request.session.isRegex, 
+				request.session.pctVisitors,
+				request.session.varTitle,
+				request.session.varPercent,
+				request.session.customJS,
+				request.session.startTime,
+				request.session.stopTime);
+
+
+				break;
+			}
+
 		}
 
 	});
